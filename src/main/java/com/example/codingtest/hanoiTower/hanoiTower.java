3d6b831fc361c56f20ff8d50f5c1733ca4223240
laegel <laegel@selper.co.kr>
2022-10-14 18:31:28 +0900

    Title: 하노이의 탑 문제풀이

---
+++ src/main/java/com/example/codingtest/hanoiTower/hanoiTower.java
@@ -0,0 +1,39 @@
+package com.example.codingtest.hanoiTower;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * https://www.acmicpc.net/problem/11729
+ */
+public class hanoiTower {
+    static StringBuilder sb = new StringBuilder();
+    static int count = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int number = Integer.parseInt(br.readLine());
+
+        hanoi(number, 1, 3, 2);
+
+        System.out.println(count);
+        System.out.println(sb);
+    }
+
+    public static void move(int start, int to) {
+        sb.append(start).append(" ").append(to).append("\n");
+    }
+
+    public static void hanoi(int number, int start, int to, int via) {
+        count++;
+
+        if (number == 1) {
+            move(start, to);
+        } else {
+            hanoi(number - 1, start, via, to);
+            move(start, to);
+            hanoi(number - 1, via, to, start);
+        }
+    }
+}
