7168f5c46297b0f2d3b3df8564a0ee937d575cf4
laegel <laegel@selper.co.kr>
2022-10-17 20:40:20 +0900

    Title: 백준 덩치 문제풀이

--- src/main/java/com/example/codingtest/body/Main.java
+++ src/main/java/com/example/codingtest/body/Main.java
@@ -8,48 +8,55 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.StringTokenizer;
 
+/**
+ * https://www.acmicpc.net/problem/7568
+ */
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
         int number = Integer.parseInt(br.readLine());
-        int[] arrWeight = new int[number];
-        int[] arrHeight = new int[number];
+        int[][] arrPeople = new int[number][2];
 
 
         for (int i = 0; i < number; i++) {
             st = new StringTokenizer(br.readLine());
 
-            arrWeight[i] = Integer.parseInt(st.nextToken());
-            arrHeight[i] = Integer.parseInt(st.nextToken());
+            arrPeople[i][0] = Integer.parseInt(st.nextToken());
+            arrPeople[i][1] = Integer.parseInt(st.nextToken());
         }
 
+        int[] arrLank = getRank(arrPeople, number);
+
+        for (int i = 0; i < arrLank.length; i++) {
+            sb.append(arrLank[i]).append(" ");
+        }
+
+        System.out.println(sb);
+        br.close();
     }
 
-    public static void getLank(int[] arrWeight, int[] arrHeight, int number) {
-        int[] arrLank = new int[number];
+    public static int[] getRank(int[][] arrPeople, int number) {
+        int[] arrRank = new int[number];
 
-        arrLank[0] = 1;
         for (int i = 0; i < number; i++) {
-            while (arrLank[i] == 0) {
-                for (int j = 0; j < i; j++) {
-                    if (arrWeight[i] == arrWeight[j] && arrHeight[i] == arrHeight[j]) {
-                        arrLank[j]++;
-                        arrLank[i] = arrLank[j];
-                    }
-                    //
-                    if (arrWeight[i] > arrWeight[j] && arrHeight[i] > arrHeight[j]) {
-                        arrLank[i] = arrLank[j];
-                        arrLank[j]++;
-                    }
+            int rank = 1;
 
-                    if (arrWeight[i] > arrWeight[j] && arrHeight[i] < arrHeight[j]) {
-                        arrLank[j]++;
-                        arrLank[i] = arrLank[j];
-                    }
+            for (int j = 0; j < number; j++) {
+                if (i == j) {
+                    continue;
                 }
+
+                if (arrPeople[i][0] < arrPeople[j][0] && arrPeople[i][1] < arrPeople[j][1]) {
+                    rank++;
+                }
+
+                arrRank[i] = rank;
             }
         }
+
+        return arrRank;
     }
 }
