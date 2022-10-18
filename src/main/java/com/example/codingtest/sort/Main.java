4623e4e8e47d823e5f0f9be12a5cc79b5e3e2724
laegel <laegel@selper.co.kr>
2022-10-18 14:13:31 +0900

    Title: 백준 - '수 정렬하기' 문제풀이

---
+++ src/main/java/com/example/codingtest/sort/Main.java
@@ -0,0 +1,44 @@
+package com.example.codingtest.sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * https://www.acmicpc.net/problem/2750
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int number = Integer.parseInt(br.readLine());
+
+        boolean[] arrBool = new boolean[1001];
+        boolean[] arrMinusBool = new boolean[1001];
+
+        for (int i = 0; i < number; i++) {
+            int n = Integer.parseInt(br.readLine());
+
+            if (n < 0) {
+                arrMinusBool[Math.abs(n)] = true;
+            } else {
+                arrBool[n] = true;
+            }
+        }
+
+        for (int i = 1000; i >= 0; i--) {
+            if (arrMinusBool[i]) {
+                sb.append(i * -1).append("\n");
+            }
+        }
+
+        for (int i = 0; i < 1001; i++) {
+            if (arrBool[i]) {
+                sb.append(i).append("\n");
+            }
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+}
