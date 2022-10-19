34c865f4c2e36a228a41a19b068290fe1249ebe3
laegel <laegel@selper.co.kr>
2022-10-19 15:18:50 +0900

    Title: 백준 - '나이순 정렬' 문제풀이 다른 방법으로도 풀어보기 - 객체 생성 - StringBuilder 를 이용한 counting sort

---
+++ src/main/java/com/example/codingtest/ageSort/Main2.java
@@ -0,0 +1,54 @@
+package com.example.codingtest.ageSort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * 객체 이용하여 풀어보기
+ */
+public class Main2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int number = Integer.parseInt(br.readLine());
+        Person[] person = new Person[number];
+
+        for (int i = 0; i < number; i++) {
+            st = new StringTokenizer(br.readLine());
+            person[i] = new Person(Integer.parseInt(st.nextToken()), st.nextToken());
+        }
+
+        Arrays.sort(person, (e1, e2) -> {
+            return e1.age - e2.age;
+        });
+
+        for (int i = 0; i < number; i++) {
+            sb.append(person[i].toString()).append("\n");
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+}
+
+class Person {
+    int age;
+    String name;
+
+    public Person(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return this.age + " " + this.name;
+    }
+
+}
+
+
---
+++ src/main/java/com/example/codingtest/ageSort/Main3.java
@@ -0,0 +1,38 @@
+package com.example.codingtest.ageSort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * StringBuilder 를 이용하여 counting sort.
+ */
+public class Main3 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int number = Integer.parseInt(br.readLine());
+        StringBuilder[] sbArr = new StringBuilder[201];
+
+        for (int i = 0; i < number; i++) {
+            st = new StringTokenizer(br.readLine());
+            int age = Integer.parseInt(st.nextToken());
+
+            if (sbArr[age] == null) {
+                sbArr[age] = new StringBuilder();
+            } else {
+                sbArr[age].append("\n");
+            }
+            sbArr[age].append(age + " ").append(st.nextToken());
+        }
+
+        for (int i = 0; i < 201; i++) {
+            if (sbArr[i] != null) {
+                System.out.println(sbArr[i]);
+            }
+        }
+
+        br.close();
+    }
+}
