5658bebe491355061c1fdaf9f0fa6d521f1c849d
laegel <laegel@selper.co.kr>
2022-10-20 14:37:10 +0900

    Title: 백준 - 'N과 M(2)' 문제풀이

---
+++ src/main/java/com/example/codingtest/nAndM_2/Main.java
@@ -0,0 +1,55 @@
+package com.example.codingtest.nAndM_2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] arrInt;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int number = Integer.parseInt(st.nextToken());
+        int length = Integer.parseInt(st.nextToken());
+
+        arrInt = new int[length];
+        visited = new boolean[number];
+
+        dfs(number, length, 0, 0);
+
+        System.out.println(sb);
+        br.close();
+    }
+
+    public static void dfs(int number, int length, int start, int depth) {
+        if (depth == length) {
+            for (int i = 0; i < length; i++) {
+                sb.append(arrInt[i]+ " ");
+            }
+
+            sb.append("\n");
+
+            return;
+        }
+
+
+        for (int i = start; i < number; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                arrInt[depth] = i + 1;
+                depth++;
+                start = i;
+
+                dfs(number, length, start, depth);
+
+                depth--;
+                visited[i] = false;
+
+            }
+        }
+    }
+}
