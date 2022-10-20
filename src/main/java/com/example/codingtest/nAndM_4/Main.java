7d65d32e6c4f8274cb8cd19040763db28bd3f2c1
laegel <laegel@selper.co.kr>
2022-10-20 15:13:56 +0900

    Title: 백준 - 'N과 M(4)' 문제풀이

---
+++ src/main/java/com/example/codingtest/nAndM_4/Main.java
@@ -0,0 +1,44 @@
+package com.example.codingtest.nAndM_4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static int[] arrInt;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int number = Integer.parseInt(st.nextToken());
+        int length = Integer.parseInt(st.nextToken());
+        arrInt = new int[length];
+
+        dfs(number, length, 0, 0);
+
+        System.out.println(sb);
+        br.close();
+    }
+
+    public static void dfs(int number, int length, int start, int depth) {
+        if (depth == length) {
+            for (int i : arrInt) {
+                sb.append(i + " ");
+            }
+
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = start; i < number; i++) {
+            arrInt[depth] = i + 1;
+
+            dfs(number, length, start, depth + 1);
+            start++;
+        }
+    }
+
+
+}
