93667b97fc38d64cba8ebe84f54d9ab14535ba0a
laegel <laegel@selper.co.kr>
2022-11-04 12:10:32 +0900

    Title : 백준 '블랙잭' 문제풀이

--- src/main/java/com/example/codingtest/boj2798_블랙잭/Main2.java
+++ src/main/java/com/example/codingtest/boj2798_블랙잭/Main2.java
@@ -6,20 +6,43 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main2 {
-    static int[] CARD;
-    static int MAX = Integer.MIN_VALUE;
+    static int SCORE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int number = Integer.parseInt(st.nextToken());
-        int score = Integer.parseInt(st.nextToken());
+        SCORE = Integer.parseInt(st.nextToken());
+        int[] card = new int[number];
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < number; i++) {
-            CARD[i] = Integer.parseInt(st.nextToken());
+            card[i] = Integer.parseInt(st.nextToken());
         }
 
+        System.out.println(find(card, number));
+        br.close();
+    }
+
+    public static int find(int[] card, int number) {
+        int result = Integer.MIN_VALUE;
+
+        for (int i = 0; i < number - 2; i++) {
+            for (int j = i + 1; j < number - 1; j++) {
+                for (int k = j + 1; k < number; k++) {
+                    int temp = card[i] + card[j] + card[k];
+                    if (temp == SCORE) {
+                        return temp;
+                    }
+
+                    if (temp <= SCORE) {
+                        result = Math.max(result, temp);
+                    }
+                }
+            }
+        }
+
+        return result;
     }
 
 
