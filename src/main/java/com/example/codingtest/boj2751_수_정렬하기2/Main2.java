a4f4a574b510cfb82c4d1718698459da75693861
laegel <laegel1@gmail.com>
2022-11-08 13:40:26 +0900

    Title : 백준 '수 정렬하기2' 문제풀이

--- src/main/java/com/example/codingtest/boj2751_수_정렬하기2/Main2.java
+++ src/main/java/com/example/codingtest/boj2751_수_정렬하기2/Main2.java
@@ -1,28 +1,24 @@
-package com.example.codingtest.sort2;
+package com.example.codingtest.boj2751_수_정렬하기2;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/**
- * https://www.acmicpc.net/problem/2751
- */
-public class Main {
+public class Main2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+        int[] countingSort = new int[2000001];
         int number = Integer.parseInt(br.readLine());
 
-        boolean[] arrBool = new boolean[2000001];
-
-        while (number-- > 0) {
-            int n = Integer.parseInt(br.readLine());
-            arrBool[n+1000000] = true;
+        for (int i = 0; i < number; i++) {
+            countingSort[Integer.parseInt(br.readLine()) + 1000000]++;
         }
 
-        for (int i = 0; i < arrBool.length; i++) {
-            if (arrBool[i]) {
+        for (int i = 0; i < countingSort.length; i++) {
+            while (countingSort[i] > 0) {
                 sb.append(i - 1000000).append("\n");
+                countingSort[i]--;
             }
         }
 
